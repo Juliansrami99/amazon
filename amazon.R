6bfa4a41d951183aa0a20fadf75bc9339e6d6083
Julian Ramirez <julians.ramirez@urosario.edu.co>
2020-07-29 22:04:28 -0500

    spy

--- amazon.R
+++ amazon.R
@@ -1084,7 +1084,7 @@ con <- dbConnect(drv     = RMySQL::MySQL(),
 		dbname   = "macrowise")
 
 for (i in 1:length(lista_accion)){
-   dbWriteTable(con,paste0(vec[i],"_spy"), lista_accion[[i]])
+   dbWriteTable(con,paste0(vec[i],"uspy"), lista_accion[[i]])
 }
 
 
